5ba2336e0fc5243a941db16ee07a9ab780a3a947
Riku-O <baskelife-rikur11@ezweb.ne.jp>
2021-11-20 16:27:58 +0900

    rails g modelをする際記述ミスがあったので、rails dで削除後に作り直し

--- db/migrate/20211120072636_create_comments.rb
+++ db/migrate/20211120072636_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
-      t.refernces :board
+      t.references :board, foreign_key: true
       t.string :name
       t.text :comment
 
