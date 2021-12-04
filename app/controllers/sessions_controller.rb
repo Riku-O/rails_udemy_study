0444d3fee3f8f4f818824efe4e572383c4bd7d9c
Riku-O <baskelife-rikur11@ezweb.ne.jp>
2021-12-04 20:47:36 +0900

    ログインユーザーの取得とマイページの作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
     user = User.find_by(name: params[:session][:name])
-    if user && user.authenicate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       redirect_to mypage_path
     else
