3fdd37285342ad874f49ae1b2ef3a4297c28f426
Riku-O <baskelife-rikur11@ezweb.ne.jp>
2021-12-06 03:33:27 +0900

    rspecの導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect_to mypage_path
     else
-      redirect_to :back, flash: {
+      redirect_back fallback_location: root_path, flash: {
         user: user,
         error_messages: user.errors.full_messages
       }
@@ -23,5 +23,5 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :password, :password_confirmation)
-    end 
+    end
 end
